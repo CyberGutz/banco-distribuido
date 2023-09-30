13c90ca5163845d9856241d4e6477372406b9ac5
Alberto <0056119@academico.ifmg.edu.br>
2023-09-30 19:51:41 -0300

    DESGRAÇA DE LINGUAGEM

--- Server.java
+++ Server.java
@@ -1,54 +1,81 @@
 
-
-import java.rmi.*; 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.rmi.*;
 import java.rmi.server.*;
-import java.util.HashMap; 
-import java.util.Map; 
+import java.util.HashMap;
+import java.util.Map;
+import org.json.*;
 
+public class Server extends UnicastRemoteObject implements API {
 
-public class Server extends UnicastRemoteObject implements API { 
+	public static void main(String args[]) {
+		try {
 
-	public static void main(String args[]) { 
-		try { 
+			// Cria um objeto do tipo da classe CalculadoraServer.
+			//Server obj = new Server();
 
-			// Cria um objeto do tipo da classe CalculadoraServer. 
-			Server obj = new Server(); 
+			// Liga (bind) esta instancia de objeto ao nome "calc" no registro RMI no
+			// localhost
+			//Naming.bind("rmi://localhost/calc", obj);
 
-			// Liga (bind) esta instancia de objeto ao nome "calc" no registro RMI no localhost
-			Naming.bind("rmi://localhost/calc", obj); 
-			
-			// Liga (bind) esta instancia de objeto ao nome "Calculadora" em um registro RMI contido em outra máquina
-			//Naming.rebind("rmi://172.22.70.30:1099/calc", obj); 
+			// Liga (bind) esta instancia de objeto ao nome "Calculadora" em um registro RMI
+			// contido em outra máquina
+			// Naming.rebind("rmi://172.22.70.30:1099/calc", obj);
+			JSONArray jsonArray = new JSONArray(10);
+			System.out.println(jsonArray);
+			// DEBUG
+			System.out.println("Server >> ligado no registro RMI sob o nome 'calc' ");
 
-			//DEBUG
-			System.out.println("Server >> ligado no registro RMI sob o nome 'calc' "); 
-
-		} 
-		catch (Exception erro) { 
-			//DEBUG
-			System.out.println("ERRO: Server " + erro.getMessage()); 
-			erro.printStackTrace(); 
-		} 
+		} catch (Exception erro) {
+			// DEBUG
+			System.out.println("ERRO: Server " + erro.getMessage());
+			erro.printStackTrace();
+		}
 
 	}
 
-	//Construtor do objeto
-	public Server() throws RemoteException { 
-		super(); //invoca o construtor do UnicastRemoteObject
-	} 
+	// Construtor do objeto
+	public Server() throws RemoteException {
+		super(); // invoca o construtor do UnicastRemoteObject
+	}
 
-	//Implementa os métodos remotos disponibilizados pela interface Calculadora
-	public Map<String,String> criarConta(String usuario, String senha) throws RemoteException{
-		Map<String,String> retorno = new HashMap<String,String>();
-		retorno.put("erro", "Conta ja existe");
+	// Implementa os métodos remotos disponibilizados pela interface Calculadora
+	public Map<String, String> criarConta(String usuario, String senha) throws RemoteException {
+		Map<String, String> retorno = new HashMap<String, String>();
 
+		try {
+
+			JSONArray jsonArray;
+
+			//tenta ler algum arquivo se já existir
+			try (FileReader fileReader = new FileReader("users.json")) {
+				jsonArray = new JSONArray(new JSONTokener(fileReader));
+			} catch (IOException e) {
+				jsonArray = new JSONArray();
+			}
+
+			JSONObject user = new JSONObject();
+			user.put("usuario", usuario);
+			user.put("senha", senha);
+
+			jsonArray.put(user);
+
+			FileWriter fileWriter = new FileWriter("users.json"); 
+			jsonArray.write(fileWriter);
+
+			retorno.put("token","zazaza");
+		} catch (Exception e) {
+			retorno.put("erro", e.getMessage());
+		}
 		return retorno;
 	}
-	
-	public Map<String,String> fazerLogin(String usuario, String senha) throws RemoteException{
-		Map<String,String> retorno = new HashMap<String,String>();
+
+	public Map<String, String> fazerLogin(String usuario, String senha) throws RemoteException {
+		Map<String, String> retorno = new HashMap<String, String>();
 		retorno.put("token", "1234");
-	
+
 		return retorno;
 	}
 
--- run.ps1
+++ run.ps1
@@ -1,13 +1,15 @@
 start rmiregistry
 sleep 2
 
-javac API.java 
-javac Client.java  
-javac Server.java
+set CLASSPATH=lib/json.jar
+
+javac src/API.java 
+javac src/Client.java  
+javac src/Server.java
 
 #Inicia server em outro terminal
-Start-Process -FilePath "cmd.exe" -ArgumentList "/k java Server"
+Start-Process -FilePath "cmd.exe" -ArgumentList "/k java src/Server"
 #A mimir alguns segundos pro server iniciar a tempo do client abrir
 sleep 2
 #Inicia client em outro terminal
-Start-Process -FilePath "cmd.exe" -ArgumentList "/k java Client"
+Start-Process -FilePath "cmd.exe" -ArgumentList "/k java src/Client"
